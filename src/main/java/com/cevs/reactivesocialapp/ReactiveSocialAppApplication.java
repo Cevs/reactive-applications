b34e013f0d565f1006aa03aa3d2b967cca704d78
Alen Martinčević <martincevic.alen.development@gmail.com>
2019-03-28 16:05:32 +0100

    Initialize project
    Dependencies: Reactive Web, Reactive MongoDB, Thymeleaf, Lombok and DevTools

---
+++ src/main/java/com/cevs/reactivesocialapp/ReactiveSocialAppApplication.java
@@ -0,0 +1,13 @@
+package com.cevs.reactivesocialapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSocialAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSocialAppApplication.class, args);
+	}
+
+}

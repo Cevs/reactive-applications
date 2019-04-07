8c4808d5814b2541ec98f063d6fbb3c1091c45eb
Alen Martinčević <martincevic.alen.development@gmail.com>
2019-04-07 22:34:30 +0200

    Add Spring cloud stream (with RabbitMQ)

--- src/main/java/com/cevs/reactivesocialapp/comments/CommentController.java
+++ src/main/java/com/cevs/reactivesocialapp/comments/CommentController.java
@@ -1,34 +1,49 @@
 package com.cevs.reactivesocialapp.comments;
 
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.cloud.stream.reactive.FluxSender;
+import org.springframework.cloud.stream.reactive.StreamEmitter;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.Mono;
 
+
 @Controller
+@EnableBinding(Source.class)
 public class CommentController {
 
-    private final RabbitTemplate rabbitTemplate;
-    private final CommentWriterRepository commentRepository;
+    private FluxSink<Message<Comment>> commentSink;
+    // Message -> Spring abstraction for a POJO wrapped as transportable message
+    // that includes the ability to add headers and other information
+    private Flux<Message<Comment>> flux;
 
-    public CommentController(RabbitTemplate rabbitTemplate, CommentWriterRepository commentRepository) {
-        this.rabbitTemplate = rabbitTemplate;
-        this.commentRepository = commentRepository;
+    public CommentController() {
+        this.flux = Flux.<Message<Comment>>create(
+                emitter -> this.commentSink = emitter,
+                FluxSink.OverflowStrategy.IGNORE
+        ).publish().autoConnect();
     }
 
     @PostMapping("/comments")
     public Mono<String> addComment(Mono<Comment> newComment){
-        return newComment
-                .flatMap(comment ->
-                    Mono.fromRunnable(()-> rabbitTemplate
-                    .convertAndSend(
-                            "learning-reactive",
-                            "comments.new",
-                            comment
-                    )))
-                .log("commentService-publish")
-                .then(Mono.just("redirect:/"));
+        if(commentSink != null){
+            return newComment
+                    .map(comment-> commentSink.next(
+                            MessageBuilder.withPayload(comment).build()
+                    )).then(Mono.just("redirect:/"));
+        }else{
+            return Mono.just("redirect:/");
+        }
+    }
+
+    @StreamEmitter
+    public void emit(@Output(Source.OUTPUT) FluxSender output){
+        output.send(this.flux);
     }
 }
--- src/main/java/com/cevs/reactivesocialapp/comments/CommentService.java
+++ src/main/java/com/cevs/reactivesocialapp/comments/CommentService.java
@@ -1,18 +1,15 @@
 package com.cevs.reactivesocialapp.comments;
 
-import com.cevs.reactivesocialapp.images.CommentReaderRepository;
-import org.springframework.amqp.rabbit.annotation.Exchange;
-import org.springframework.amqp.rabbit.annotation.Queue;
-import org.springframework.amqp.rabbit.annotation.QueueBinding;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
+@EnableBinding(CustomProcessor.class)
 public class CommentService {
 
     private CommentWriterRepository commentRepository;
@@ -21,20 +18,13 @@ public class CommentService {
         this.commentRepository = commentRepository;
     }
 
-    @RabbitListener(bindings = @QueueBinding(
-            value = @Queue,
-            exchange = @Exchange(value = "learning-reactive"),
-            key = "comments.new"
-    ))
-    public void save(Comment newComment){
-        commentRepository
-                .save(newComment)
+    @StreamListener
+    @Output(CustomProcessor.OUTPUT)
+    public Flux<Void> save(@Input(CustomProcessor.INPUT) Flux<Comment> newComments){
+        return commentRepository
+                .saveAll(newComments)
                 .log("commentService-save")
-                .subscribe();
-    }
-
-    @Bean
-    Jackson2JsonMessageConverter jackson2JsonMessageConverter(){
-        return new Jackson2JsonMessageConverter();
+                //.thenMany(Mono.empty());
+                .flatMap(comment -> Mono.empty());
     }
 }

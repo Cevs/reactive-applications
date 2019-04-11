463c12a7c45ed1e12e6c6cf3157eb3908ee34535
Alen Martinčević <martincevic.alen.development@gmail.com>
2019-04-11 21:59:05 +0200

    Remove tests

--- images/src/test/java/com/cevs/reactivesocialapp/EmbeddedImageRepositoryTests.java
+++
@@ -1,8 +0,0 @@
-package com.cevs.reactivesocialapp;
-
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@DataMongoTest
-public class EmbeddedImageRepositoryTests extends  ImageRepositoryTest{}

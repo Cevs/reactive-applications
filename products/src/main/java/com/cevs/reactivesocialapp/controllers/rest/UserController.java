254ebb900b75dfc9b65a8b046554bc8ca9bbce53
Alen Martinčević <martincevic.alen@gmail.com>
2019-04-17 17:24:47 +0200

    Add user controller

--- products/src/main/java/com/cevs/reactivesocialapp/controllers/rest/UserController.java
+++ products/src/main/java/com/cevs/reactivesocialapp/controllers/rest/UserController.java
@@ -1,4 +1,21 @@
 package com.cevs.reactivesocialapp.controllers.rest;
 
+import com.cevs.reactivesocialapp.domain.User;
+import com.cevs.reactivesocialapp.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/user/{userId}")
+    public Mono<User> getUser(@PathVariable long userId){
+        return userService.getUser(userId);
+    }
 }

b74bcd7b7a28d5d770700d0b9b818059d89af146
Alen Martinčević <martincevic.alen.development@gmail.com>
2019-04-17 23:43:20 +0200

    Finish ajax updating review section

--- products/src/main/resources/static/js/product.js
+++ products/src/main/resources/static/js/product.js
@@ -42,64 +42,50 @@ $(document).ready(function(){
         userId = reviewData.userId;
         date = reviewData.date;
         comment = reviewData.comment;
-        console.log(comment);
-
         $.get( "http://localhost:8080/user/"+userId, function( userData ) {
 
-            console.log(userData);
-            console.log(userData.imageName);
-            var $outerDiv = $("<div id=\"userReviewRow\" class=\"row no-gutters p-1\">");
-
-            var $innerDiv1 =$(
-                "<div class='col-md-2  align-self-center'>" +
-                    "<img th:src=\"@{\'/products/" + userData.imageName + "/raw\'}\"  class=\'card-img mx-auto p-1\ style=\'height:80%;\'>" +
-                    "<p style=\'font-size:12px; margin:0\' th:text=\'"+userData.username+"\'></p>"+
-                    "<p class=\'card-text\'><small class=\'text-muted\' style=\'font-size:12px;\' th:text=\'"+date+"\'></small></p>"+
-                "</div>");
-
-
-            var $innerDiv2 =$(
-                "<div class=\"col-md-10  align-self-center\">" +
-                    " <div class=\"card-body pt-0 pb-0\">" +
-                        "<p class=\'card-text\'  style=\'font-size:14px;\' th:text=\'"+reviewData.comment+"\'></p>"+
+            var $div =$(
+                "<div id=\"userInfo-\" class=\"card mb-3\">"+
+                    "<div class=\"row no-gutters p-1\">"+
+                        "<div class='col-md-2  align-self-center'>" +
+                            "<img src=\"/products/" + userData.imageName + "/raw\"  class=\'card-img mx-auto p-1\ style=\'height:80%;\'>" +
+                            "<p style=\'font-size:12px; margin:0\'>"+userData.username+"</p>"+
+                            "<p class=\'card-text\'><small class=\'text-muted\' style=\'font-size:12px;\'>"+date+"</small></p>"+
+                        "</div>"+
+                        "<div class=\"col-md-10  align-self-center\">" +
+                            "<div class=\"card-body pt-0 pb-0\">" +
+                                "<p class=\'card-text\'  style=\'font-size:14px;\'>"+reviewData.comment+"</p>"+
+                            "</div>"+
+                        "</div>"+
                     "</div>"+
-                "</div>");
+                "</div>"
+            );
 
-            completeDiv = $outerDiv.append($innerDiv1);
-            completeDiv.append($innerDiv2);
-            $("#userReviewSection").append(completeDiv);
+            $div.effect("highlight", {}, 2000);
+            $("#userReviewSection").append($div);
         });
-
     }
 
-    // Update button UI after adding product
-    document.querySelectorAll('.button-review')
-        .forEach(function (button) {
-            button.addEventListener('click', function(){
-                var review = document.getElementById(
-                    'review-' + button.id
-                );
+    $(".button-review").on("click", function(){
+        button = $(".button-review");
+        review = $("#reviewText").val();
+        today = new Date();
+        dd = String(today.getDate()).padStart(2,'0');
+        mm = String(today.getMonth()).padStart(2,'0');
+        yyyy = String(today.getFullYear());
+        date = yyyy+"-"+mm+"-"+dd;
 
-                var today = new Date();
-                var dd = String(today.getDate()).padStart(2,'0');
-                var mm = String(today.getMonth()).padStart(2,'0');
-                var yyyy = String(today.getFullYear());
-                var date = yyyy+"-"+mm+"-"+dd;
-
-                var xhr = new XMLHttpRequest();
-                xhr.open('POST', '/reviews', true);
-
-                var formData = new FormData();
-                formData.append('userId', 11);
-                formData.append('comment', review.value);
-                formData.append('productId', button.id);
-                formData.append('date', date);
-                xhr.send(formData);
-
-                review.value = '';
-            });
-        });
-
+        xhr = new XMLHttpRequest();
+        xhr.open('POST', '/reviews', true);
+        console.log(button.attr("id"));
+        formData = new FormData();
+        formData.append('userId', 11);
+        formData.append('comment', review);
+        formData.append('productId', button.attr("id"));
+        formData.append('date', date);
+        xhr.send(formData);
+        $("#reviewText").val("");
+    });
 
 
 });

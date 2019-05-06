7380b80c8921493ebf684177d8f06c91523fcbf8
Alen Martinčević <martincevic.alen@gmail.com>
2019-05-06 16:39:51 +0200

    Update .js for chat feature. Store open chats in local storage so that user don't lose windows when changing page. Remove usage of chat fragmet.

--- shop/src/main/resources/static/js/chat.js
+++ shop/src/main/resources/static/js/chat.js
@@ -1,14 +1,32 @@
-arrayOfExistingChats = [];
-
+chatWindows = [];
 $(document).ready(function(){
+    username = $("#logged_username").text();
+    retrievedData = localStorage.getItem(username+"chatWindows");
+
+    if(retrievedData != null){
+        chatWindows = JSON.parse(retrievedData);
+        if(chatWindows.length != 0){
+            clearChatWindowsSection();
+        }
+    }
+
+    if($("#sellerUsername").length){
+        insertNewChatWindow($("#sellerUsername").text());
+    }
+
+    refreshChatWindows();
 
     outboundChatMessage = null;
     inboundChatMessages = null;
-    username = $("#logged_username").text()
+
+    $("#btn_logout").on("click",function(){
+        localStorage.removeItem(username+"chatWindows");
+    });
+
     if((outboundChatMessage == null || inboundChatMessages == null) && username != ""){
         seller = $("#sellerUsername").text();
         if(seller != '' || seller == 'undefined'){
-            arrayOfExistingChats.push(seller);
+            insertNewChatWindow(seller);
         }
         outboundChatMessage = new WebSocket('ws://localhost:8200/app/chatMessage.new?user='
             + username);
@@ -26,6 +44,7 @@ $(document).ready(function(){
                 stringReplay = JSON.stringify(jsonMessage);
 
                 if(!isEmpty(reply)){
+                    insertNewChatWindow(seller);
                     outboundChatMessage.send(stringReplay);
                     $("#reply-"+seller).val("");
 
@@ -50,87 +69,19 @@ $(document).ready(function(){
                 messageText = message.message;
 
                 /*Check if chat already exists */
-                if(username != fromUser && (jQuery.inArray(fromUser, arrayOfExistingChats) == -1)){
-                    arrayOfExistingChats.push(fromUser); //Push chat in chatArray
-
-                    numberOfChatBoxes = arrayOfExistingChats.length;
-
-                    positionRight = 20 + (numberOfChatBoxes-1)*320
-                    //Create chat box
-                    var $chatBox =  $(
-                        "<div id=\"chatwindow-"+fromUser+"\"class=\"chatwindow\" style='right:"+positionRight+"px'>"+
-                        "<div id=\"chatheader-"+fromUser+"\" class=\"chatheader\">"+
-                        "<div id=\"close-"+fromUser+"\" class=\"close-btn\"><i id=\"chatHeaderButton-"+fromUser+"\" class=\"far fa-times-circle\"></i></div>"+
-                        "<div class=\"title\"><span>"+fromUser+"</span></div>"+
-                        "</div>"+
-                        "<div id=\"container-"+fromUser+"\" class=\"container-chat\">"+
-                        "<div id=\"chatDisplay-"+fromUser+"\" class=\"chatDisplay conversations\" disabled=\"true\" style=\"overflow-y: auto; height:250px;\"></div>"+
-                        "</div>"+
-                        "<div id=\"replySection-"+fromUser+"\" class=\"replySection\">"+
-                        "<textarea name=\"reply\" id=\"reply-"+fromUser+"\" cols=\"30\" rows=\"1\" class=\"reply\"></textarea>"+
-                        "<button id=\"send-"+fromUser+"\" class=\"btn-send\" >Send</button>"+
-                        "</div>"+
-                        "</div>"
-                    );
-
-                    //Append chatbox
-                    $("#chatSection").append($chatBox);
-
-                    //bind functions
-                    $("#chatheader-"+fromUser).on("click",function(event){
-                        if($("#container-"+fromUser).is(":visible")){
-                            $("#container-"+fromUser).hide();
-                            $("#replySection-"+fromUser).hide();
-                        }else{
-                            $("#container-"+fromUser).show();
-                            $("#replySection-"+fromUser).show();
-                        }
-                    });
-
-                    $("#close-"+fromUser).on("click",function(event){
-                        user = (event.currentTarget.id).substring(6); //Remove 'close-' to get username
-                        pos = $.inArray(user, arrayOfExistingChats);
-                        if(pos != -1){
-                            $("#chatwindow-"+user).remove();
-                            arrayOfExistingChats.splice($.inArray(pos,1));
-                        }
-
-
-                    });
-
-                    $("#send-"+fromUser).on("click", function (event) {
-                        user = (event.currentTarget.id).substring(5); //Remove 'send-' to get username
-                        reply = $("#reply-"+user).val();
-                        jsonMessage = {
-                            sender: username,
-                            receiver: user,
-                            message: reply
-                        };
-
-                        stringReplay = JSON.stringify(jsonMessage);
-
-                        if(!isEmpty(reply)){
-                            outboundChatMessage.send(stringReplay);
-                            //Update chat display
-                            chatDisplay = $("#chatDisplay-"+user);
-                            $div = "<p class='message'><span class='send'>"+reply+"</span></p>"
-                            chatDisplay.append($div);
-                            $("#reply-"+user).val("");
-                        }
-                    });
+                if(username != fromUser && (jQuery.inArray(fromUser, chatWindows) == -1)){
+                    insertNewChatWindow(fromUser);
+                    refreshChatWindows();
                 }
 
-                //Sender is user so we update chat for reciever with sender class
-                if(username == fromUser){
-                    //Do nothing, already updated
-                }else if(username == toUser){ //MI smo primatelj poruke
+               if(username == toUser){
                     chatDisplay = $("#chatDisplay-"+fromUser);
                     $div = "<p class='message'><span class='receive'>"+messageText+"</span></p>"
                     chatDisplay.append($div);
                 }
             }
 
-        };//Kraj inbound messagea
+        };
 
         $("#reply").focus();
 
@@ -148,9 +99,110 @@ $(document).ready(function(){
     });
 });
 
-
-
 function isEmpty(value) {
     return typeof value == 'string' && !value.trim() || typeof value == 'undefined' || value === null;
+}
+
+
+function insertNewChatWindow(username){
+    if(!chatWindowExists(username)){
+        chatWindows.push(username);
+        updateLocalStorage();
+    }
+}
+
+function chatWindowExists(username){
+    return chatWindows.includes(username);
+}
+
+function removeChatWindow(username){
+    if(chatWindowExists(username)){
+        for( var i = 0; i < chatWindows.length; i++){
+            if ( chatWindows[i] === username) {
+                chatWindows.splice(i, 1);
+            }
+        }
+        updateLocalStorage();
+        refreshChatWindows();
+    }
+}
+
+function updateLocalStorage(){;
+    localStorage.setItem(username+"chatWindows", JSON.stringify(chatWindows));
+}
+
+function clearChatWindowsSection(){
+    $("#chatSection").empty();
+}
+
+/*
+    Load opened chat windows with other users
+ */
+function refreshChatWindows(){
+    clearChatWindowsSection();
+    for(var i = 0; i<chatWindows.length; i++){
+        var user = chatWindows[i];
+        positionRight = 20 + i * 320;
+        var $chatBox =  $(
+            "<div id=\"chatwindow-"+user+"\"class=\"chatwindow\" style='right:"+positionRight+"px'>"+
+            "<div id=\"chatheader-"+user+"\" class=\"chatheader\">"+
+            "<div id=\"close-"+user+"\" class=\"close-btn\"><i id=\"chatHeaderButton-"+user+"\" class=\"far fa-times-circle\"></i></div>"+
+            "<div class=\"title\"><span>"+user+"</span></div>"+
+            "</div>"+
+            "<div id=\"container-"+user+"\" class=\"container-chat\">"+
+            "<div id=\"chatDisplay-"+user+"\" class=\"chatDisplay conversations\" disabled=\"true\" style=\"overflow-y: auto; height:250px;\"></div>"+
+            "</div>"+
+            "<div id=\"replySection-"+user+"\" class=\"replySection\">"+
+            "<textarea name=\"reply\" id=\"reply-"+user+"\" cols=\"30\" rows=\"1\" class=\"reply\"></textarea>"+
+            "<button id=\"send-"+user+"\" class=\"btn-send\" >Send</button>"+
+            "</div>"+
+            "</div>"
+        );
+
+        //Append chatbox
+        $("#chatSection").append($chatBox);
+
+
+        //bind functions
+        $("#chatheader-"+user).on("click",function(event){
+            if($("#container-"+user).is(":visible")){
+                $("#container-"+user).hide();
+                $("#replySection-"+user).hide();
+            }else{
+                $("#container-"+user).show();
+                $("#replySection-"+user).show();
+            }
+        });
+
+        $("#close-"+user).on("click",function(event){
+            user = (event.currentTarget.id).substring(6); //Remove 'close-' to get username
+            pos = $.inArray(user, chatWindows);
+            if(pos != -1){
+                $("#chatwindow-"+user).remove();
+                removeChatWindow(user);
+            }
+        });
+
+        $("#send-"+user).on("click", function (event) {
+            user = (event.currentTarget.id).substring(5); //Remove 'send-' to get username
+            reply = $("#reply-"+user).val();
+            jsonMessage = {
+                sender: username,
+                receiver: user,
+                message: reply
+            };
+
+            stringReplay = JSON.stringify(jsonMessage);
+
+            if(!isEmpty(reply)){
+                outboundChatMessage.send(stringReplay);
+                //Update chat display
+                chatDisplay = $("#chatDisplay-"+user);
+                $div = "<p class='message'><span class='send'>"+reply+"</span></p>"
+                chatDisplay.append($div);
+                $("#reply-"+user).val("");
+            }
+        });
+    }
 }
 
